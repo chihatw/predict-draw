f258e77f0f98741d1dc90ada36a6de0f51261a77
chihatw <chiha.tw@gmail.com>
2022-03-02 15:41:44 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material';
+
+function App() {
+  return (
+    <Container maxWidth='sm'>
+      <div>predict draw</div>
+    </Container>
+  );
+}
+
+export default App;
