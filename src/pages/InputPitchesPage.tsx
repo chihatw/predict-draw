e5d63621824b13478399e0ff16bafb0ddd3bba31
chihatw <chiha.tw@gmail.com>
2022-03-10 15:29:14 +0900

    pitchesArray2String

--- src/pages/InputPitchesPage.tsx
+++ src/pages/InputPitchesPage.tsx
@@ -1,19 +1,40 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import string2PitchesArray from 'string2pitches-array';
+import { Container, TextField } from '@mui/material';
+import pitchesArray2String from 'pitches-array2string';
+
 import Layout from '../Layout';
-import { InputPitchList } from '@chihatw/lang-gym-h.form.input-pitch-list';
-import { Button, Container } from '@mui/material';
 import AppContext from '../services/context';
 
 const InputPitchesPage = () => {
   const { handleUpdatePitchList, note1PitchList } = useContext(AppContext);
-  const [pitchListProps, setPitchListProps] = useState<{
-    pitchList: [string, string[][][]][];
-  }>({
-    pitchList: [],
-  });
 
-  const handleSubmit = () => {
-    const pitchList = pitchListProps.pitchList;
+  const [input, setInput] = useState('');
+
+  // note1PitchListを文字列化して、inputに代入
+  useEffect(() => {
+    // inputにすでに値がある場合は、代入しない
+    if (!!input) return;
+    const lines: string[] = [];
+    for (const line of note1PitchList) {
+      lines.push(line[0]);
+      lines.push(pitchesArray2String(line[1]));
+    }
+    setInput(lines.join('\n'));
+  }, [note1PitchList, input]);
+
+  const handleChangeInput = (input: string) => {
+    setInput(input);
+    const pitchList: [string, string[][][]][] = [];
+    const lines = input.split('\n').filter((i) => i);
+    lines.forEach((line, index) => {
+      if (index % 2) {
+        const i = (index - 1) / 2;
+        pitchList[i][1] = string2PitchesArray(line);
+      } else {
+        pitchList.push([line, []]);
+      }
+    });
     handleUpdatePitchList({ pitchList, note: 'note1' });
   };
 
@@ -22,20 +43,16 @@ const InputPitchesPage = () => {
       <Container maxWidth='sm'>
         <div
           style={{
-            display: 'flex',
-            justifyContent: 'center',
+            display: 'grid',
           }}
         >
-          <div style={{ display: 'grid', rowGap: 8 }}>
-            <Button
-              color='secondary'
-              variant='contained'
-              onClick={handleSubmit}
-            >
-              更新
-            </Button>
-            <InputPitchList superSetPitchListProps={setPitchListProps} />
-          </div>
+          <TextField
+            rows={10}
+            value={input}
+            color='secondary'
+            multiline
+            onChange={(e) => handleChangeInput(e.target.value)}
+          />
         </div>
       </Container>
     </Layout>
