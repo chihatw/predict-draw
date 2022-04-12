b205c025f6751ddf5995f57229e0bb57f3d73354
chihatw <chiha.tw@gmail.com>
2022-04-12 21:15:22 +0900

    ストップウォッチ追加

---
+++ src/panes/WorkoutItems/WorkoutItemsPane/components/WorkoutItemRow.tsx
@@ -0,0 +1,65 @@
+import { Check } from '@mui/icons-material';
+import { Button, Collapse } from '@mui/material';
+import React from 'react';
+import { WorkoutItem } from '../../../../services/useWorkoutItems';
+
+const WorkoutItemRow = ({
+  index,
+  isChecked,
+  workoutItem,
+  isCurrentChecked,
+  handleClick,
+}: {
+  index: number;
+  isChecked: boolean;
+  workoutItem: WorkoutItem;
+  isCurrentChecked: boolean;
+  handleClick: () => void;
+}) => {
+  return (
+    <Collapse in={!isChecked || isCurrentChecked}>
+      <Button
+        fullWidth
+        sx={{
+          color: '#555',
+          padding: '8px 16px',
+          textAlign: 'left',
+          margin: '8px 0',
+        }}
+        disabled={isChecked}
+        onClick={handleClick}
+      >
+        <div
+          style={{
+            display: 'grid',
+            rowGap: 4,
+            flexGrow: 1,
+            alignItems: 'center',
+          }}
+        >
+          <div style={{ fontSize: 14 }}>{`${index + 1}.${
+            workoutItem.text
+          }`}</div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div
+              style={{
+                flexGrow: 1,
+                fontSize: 16,
+                color: '#52a2aa',
+                background: isCurrentChecked ? 'lightyellow' : 'transparent',
+                padding: 4,
+                borderRadius: 4,
+                marginRight: 16,
+              }}
+            >
+              {workoutItem.chinese}
+            </div>
+            <Check sx={{ color: isChecked ? '#52a2aa' : '#eee' }} />
+          </div>
+        </div>
+      </Button>
+    </Collapse>
+  );
+};
+
+export default WorkoutItemRow;
--- src/panes/WorkoutItems/WorkoutItemsPane/index.tsx
+++ src/panes/WorkoutItems/WorkoutItemsPane/index.tsx
@@ -1,17 +1,43 @@
-import { Check } from '@mui/icons-material';
-import { Button, Container, Divider } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import { ClearRounded, StopCircleRounded } from '@mui/icons-material';
+import { Container, IconButton } from '@mui/material';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   useHandleWorkoutItems,
   useWorkoutItems,
-  WorkoutItem,
 } from '../../../services/useWorkoutItems';
+import TimerDisplay from './components/TimerDisplay';
+import WorkoutItemRow from './components/WorkoutItemRow';
 
 const WorkoutItemsPane = () => {
   const { workoutItems, checkedIndexes: superCheckedIndexes } =
     useWorkoutItems();
   const { setCheckedIndexes: superSetCheckedIndexes } = useHandleWorkoutItems();
   const [checkedIndexes, setCheckedIndexes] = useState<number[]>([]);
+  const [currentCheckedIndex, setCurrentCheckedIndex] = useState(-1);
+
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+  const startAtRef = useRef(0);
+  const rafRef = useRef(0);
+
+  const start = () => {
+    startAtRef.current = performance.now();
+    loop();
+    setIsRunning(true);
+  };
+
+  const loop = () => {
+    const elapsedTime = performance.now() - startAtRef.current;
+    setTime(elapsedTime);
+    rafRef.current = window.requestAnimationFrame(loop);
+  };
+
+  const stop = () => {
+    const elapsedTime = performance.now() - startAtRef.current;
+    setTime(elapsedTime);
+    window.cancelAnimationFrame(rafRef.current);
+    setIsRunning(false);
+  };
 
   useEffect(() => {
     setCheckedIndexes(superCheckedIndexes);
@@ -21,17 +47,28 @@ const WorkoutItemsPane = () => {
     const newCheckedIndexes = [...checkedIndexes];
     newCheckedIndexes.push(index);
     setCheckedIndexes(newCheckedIndexes);
+    setCurrentCheckedIndex(index);
     superSetCheckedIndexes(newCheckedIndexes);
+    if (newCheckedIndexes.length === 1) {
+      start();
+    }
   };
 
   const handleClickReset = () => {
     setCheckedIndexes([]);
     superSetCheckedIndexes([]);
+    setCurrentCheckedIndex(-1);
+    setTime(0);
+  };
+
+  const handleClickStop = () => {
+    stop();
   };
 
   return (
-    <Container maxWidth='sm' sx={{ paddingTop: 5 }}>
-      <div style={{ display: 'grid', rowGap: 16, padding: '0 16px' }}>
+    <Container maxWidth='sm'>
+      <TimerDisplay time={time} />
+      <div style={{ display: 'grid', rowGap: 0 }}>
         {workoutItems.map((workoutItem, index) => (
           <WorkoutItemRow
             key={index}
@@ -39,65 +76,25 @@ const WorkoutItemsPane = () => {
             workoutItem={workoutItem}
             handleClick={() => handleClickCheck(index)}
             isChecked={checkedIndexes.includes(index)}
+            isCurrentChecked={currentCheckedIndex === index}
           />
         ))}
-        <Divider />
-        <Button
-          variant='contained'
-          sx={{ color: 'white' }}
-          onClick={handleClickReset}
-        >
-          リセット
-        </Button>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {isRunning ? (
+            <IconButton onClick={handleClickStop}>
+              <StopCircleRounded sx={{ fontSize: 120, color: '#52a2aa' }} />
+            </IconButton>
+          ) : (
+            !!checkedIndexes.length && (
+              <IconButton onClick={handleClickReset}>
+                <ClearRounded sx={{ fontSize: 120, color: '#52a2aa' }} />
+              </IconButton>
+            )
+          )}
+        </div>
       </div>
     </Container>
   );
 };
 
 export default WorkoutItemsPane;
-
-const WorkoutItemRow = ({
-  index,
-  isChecked,
-  workoutItem,
-  handleClick,
-}: {
-  index: number;
-  isChecked: boolean;
-  workoutItem: WorkoutItem;
-  handleClick: () => void;
-}) => {
-  return (
-    <Button
-      sx={{ color: '#555', background: isChecked ? '#eee' : 'transparent' }}
-      style={{
-        display: 'flex',
-
-        padding: '8px 16px',
-        borderRadius: 8,
-        textAlign: 'left',
-      }}
-      disabled={isChecked}
-      onClick={handleClick}
-    >
-      <div
-        style={{
-          display: 'grid',
-          rowGap: 4,
-          flexGrow: 1,
-          alignItems: 'center',
-        }}
-      >
-        <div style={{ fontSize: 14 }}>{`${index + 1}.${workoutItem.text}`}</div>
-        <div style={{ fontSize: 16, color: '#52a2aa' }}>
-          {workoutItem.chinese}
-        </div>
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <span style={{ color: isChecked ? '#52a2aa' : '#eee' }}>
-          <Check />
-        </span>
-      </div>
-    </Button>
-  );
-};
