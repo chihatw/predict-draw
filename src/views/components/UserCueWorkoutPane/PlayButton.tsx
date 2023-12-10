8c8e5d21d5e4f1161247bb5475a1b7f930e2f2e5
chihatw <chiha.tw@gmail.com>
2023-12-10 16:23:11 +0900

    update Playbutton rolling

--- src/views/components/UserCueWorkoutPane/PlayButton.tsx
+++ src/views/components/UserCueWorkoutPane/PlayButton.tsx
@@ -1,19 +1,18 @@
-import { css, keyframes } from '@emotion/css';
 import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
 import PlayCircleRounded from '@mui/icons-material/PlayCircleRounded';
 import { IconButton } from '@mui/material';
 import { RootState } from 'main';
 import { useSelector } from 'react-redux';
 
-const rotate = keyframes`
-  0%  {
-    transform: rotate(0deg);
-  }
+// const rotate = keyframes`
+//   0%  {
+//     transform: rotate(0deg);
+//   }
 
-  100% {
-    transform: rotate(-360deg);
-  }
-`;
+//   100% {
+//     transform: rotate(-360deg);
+//   }
+// `;
 
 const PlayButton = ({ handleClick }: { handleClick: () => void }) => {
   const { isRunning, points } = useSelector(
@@ -30,9 +29,9 @@ const PlayButton = ({ handleClick }: { handleClick: () => void }) => {
       <IconButton color='primary' onClick={handleClick}>
         {isRunning ? (
           <ChangeCircleIcon
-            className={css`
-              animation: ${rotate} 4s linear infinite;
-            `}
+            // className={css`
+            //   animation: ${rotate} 4s linear infinite;
+            // `}
             sx={{ fontSize: 120 }}
           />
         ) : points ? (
